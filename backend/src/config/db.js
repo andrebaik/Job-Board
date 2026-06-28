aa0641afca3f7eb523c1d320e1f3727ee5d391a2
andrebaikk <vicryandr499@gmail.com>
2026-06-28 11:28:21 +0700

    deploy

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -5,12 +5,10 @@ dotenv.config( {quiet: true} )
 
 const db = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
 })
 
 export default db
